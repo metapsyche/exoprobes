b2ae0ae66098ca596dd9883357a2e49a4290ade5
Metapsyche <mindrogen@gmail.com>
2021-04-25 12:27:03 -0400

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				ExoProbes
+			</title>
+			<meta name={"description"} content={"Exoprobes is a collection of fictional images transmitted by exoplanetary probes."} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://uploads.quarkly.io/6085970723a143001e08fffc/images/ExoProbes0002.jpg?v=2021-04-25T16:23:58.050Z) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--grey"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					ExoProbes
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					.
+				</Text>
+				<Text font="--base">
+					In 2082, ExoProbes were launched to survey exoplanets around nearby stars. After thousands of years, first transmissions of the ExoProbes were finally received back on Earth. This is a raw image of an exoplanet that was captured by one of the probes.
+				</Text>
+			</Box>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,32 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6085970723a143001e08ffff",
+				"6085970723a143001e090001"
+			]
+		},
+		"6085970723a143001e08ffff": {
+			"id": "6085970723a143001e08ffff",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6085970723a143001e090001": {
+			"id": "6085970723a143001e090001",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {
+			"title": "ExoProbes",
+			"description": "Exoprobes is a collection of fictional images transmitted by exoplanetary probes."
+		}
+	}
+}
